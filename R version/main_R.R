b5015e6cd4af5d1d1054cd233936e8d625cde412
Vincent Sevestre <viviseve9@gmail.com>
2022-03-10 18:39:36 +0100

    Last debug version
    Now working

--- R version/main_R.R
+++ R version/main_R.R
@@ -4,23 +4,19 @@ library(mvtnorm)
 library(matlib)
 
 mu_tau <- function(i,param){
-  mu <- param$beta[1] * data$LRT[i]^2 + param$beta[2]*data$VR[i][2] + param$beta[3] * data$Gender[i]
-  + param$beta[4] * data$School_gender[i][1] + param$beta[5] * data$School_gender[i][2] 
-  + param$beta[6] * data$School_denom[i][1] + param$beta[7] * data$School_denom[i][2]
-  + param$beta[8] * data$School_denom[i][3]
-  + param$alpha[,1] + param$alpha[,2] * data$LRT[i] + param$alpha[,3] * data$VR[i][1]
-  
+  k <- data$school[i]
+  mu <- param$beta[1] * data$LRT[i]^2 + param$beta[2]*data$VR[i,2] + param$beta[3] * data$Gender[i] + param$beta[4] * data$School_gender[i,1] + param$beta[5] * data$School_gender[i,2] + param$beta[6] * data$School_denom[i,1] + param$beta[7] * data$School_denom[i,2] + param$beta[8] * data$School_denom[i,3] + param$alpha[k,1] + param$alpha[k,2] * data$LRT[i] + param$alpha[k,3] * data$VR[i,1]
   tau <- exp(param$theta + param$phi * data$LRT[i])
-  
   return (c(mu,tau))
+  
 }
 
 mu_tau_j <- function(j,param){
-  mu <- param$beta[1] * data$LRT[data$school== j]^2 + param$beta[2]*data$VR[data$school== j][2] + param$beta[3] * data$Gender[data$school== j]
-  + param$beta[4] * data$School_gender[data$school== j][1] + param$beta[5] * data$School_gender[data$school== j][2] 
-  + param$beta[6] * data$School_denom[data$school== j][1] + param$beta[7] * data$School_denom[data$school== j][2]
-  + param$beta[8] * data$School_denom[data$school== j][3]
-  + param$alpha[,1] + param$alpha[,2] * data$LRT[data$school== j] + param$alpha[,3] * data$VR[data$school== j][1]
+  mu <- param$beta[1] * data$LRT[data$school== j]^2 + param$beta[2]*data$VR[data$school== j,2] + param$beta[3] * data$Gender[data$school== j]
+  + param$beta[4] * data$School_gender[data$school== j,1] + param$beta[5] * data$School_gender[data$school== j,2] 
+  + param$beta[6] * data$School_denom[data$school== j,1] + param$beta[7] * data$School_denom[data$school== j,2]
+  + param$beta[8] * data$School_denom[data$school== j,3]
+  + param$alpha[j,1] + param$alpha[j,2] * data$LRT[data$school== j] + param$alpha[j,3] * data$VR[data$school== j,1]
   
   tau <- exp(param$theta + param$phi * data$LRT[data$school== j])
   
@@ -29,6 +25,7 @@ mu_tau_j <- function(j,param){
 
 prop_sd <- 0.01
 Sigma <- inv(R) 
+tau <- 0.001
 mn = c(0,0,0)
 prec <- diag(3)*100
 
@@ -86,16 +83,17 @@ sample_beta <- function(k,X){
                  ,  data$School_denom[,3])
   tau_i <- 1:N
   a_i <- 1:N
-  for (i in 1:N){
+  for (i in 1:N){  
     L <- mu_tau(i,X)
     mu <- L[1]
     tau_i[i] <- L[2]
     nu_ki <- nu_k[i]
     a_i[i] <- data$Y[i] - mu + nu_ki*X$beta[k]
+    
   }
   va <- 1/(tau + sum(nu_k*tau_i))
   mu_2 <- va*(sum(nu_k*tau_i - a_i)) 
-  X$beta[k] <- rnorm(mu_2,sqrt(va))
+  X$beta[k] <- rnorm(1,mu_2,sqrt(va))
   return (X)
 }
 
@@ -103,35 +101,41 @@ sample_beta <- function(k,X){
 sample_alpha <-function(X){ # Marche aléatoire 
   alpha <- matrix(NA,M,3)
   for (j in 1:M){
-    prop <- X$alpha[,j] + rnorm(3, 0, sd=prop_sd)
-    mat_T <- rWishart(1,3,Sigma)
-    gamma <- rmvnorm(1, mn, prec)
+    prop <- X$alpha[j,] + rnorm(3, 0, sd=prop_sd)
+    mat_T <- X$T
+    gamma <- X$gamma
     A_2 <- data$LRT[data$school == j]
     A_1 <- rep(1, length(A_2))
-    A_3 <- data$VR[data$school == j][,1]
-    Aj <- c(A_1,A_2,A_3)
+    A_3 <- data$VR[data$school == j,1]
+    Aj <- matrix(NA,3,length(A_2))
+    Aj[1,] <- A_1
+    Aj[2,] <- A_2
+    Aj[3,] <- A_3
     L <- mu_tau_j(j,X)
     mu_j <- L[1]
     tau_j <- L[2]
     log_pdf <- function(alpha){
-      p1 <- -0.5*t(alpha-gamma)%*%T%*%(alpha-gamma)
-      b_j <- mu_j - alpha%*%Aj - data$Y[data$school == j]
-      p2 <- -0.5 * sum( (alpha%*%Aj)^2 - 2*b_j%*%(alpha%*%Aj)*tau_j)
+      
+      p1 <- -0.5*t(alpha-gamma)%*%mat_T%*%(alpha-gamma)
+      b_j <- sum(mu_j - alpha%*%Aj - data$Y[data$school == j])
+
+      p2 <- -0.5 * sum( ( (alpha%*%Aj)^2 - 2*b_j*(alpha%*%Aj) )*tau_j)
       return (p1 + p2)
     }
     
     top <- log_pdf(prop)
-    bottom <- log_pdf(X$alpha[,j])
+    bottom <- log_pdf(X$alpha[j,])
     acc <- exp(top - bottom)
     
-    if (runif() < acc) {
-      alpha[j] <- prop 
+
+    if (runif(1) < acc) {
+      alpha[j,] <- prop 
     }
     else {
-      alpha[j] <- X$alpha[,j]
+      alpha[j,] <- X$alpha[j,]
     }
   }
-  X$alpha <- t(alpha)
+  X$alpha <- alpha
   return (X)
 }
 
@@ -153,7 +157,7 @@ sample_theta <- function(X){
   bottom <- log_pdf(X$theta)
   acc <- exp(top - bottom)
   
-  if (runif() < acc) {
+  if (runif(1) < acc) {
     X$theta <- prop 
   }
   
@@ -178,7 +182,7 @@ sample_phi <- function(X){
   bottom <- log_pdf(X$phi)
   acc <- exp(top - bottom)
   
-  if (runif() < acc) {
+  if (runif(1) < acc) {
     X$phi <- prop 
   }
   
@@ -186,15 +190,14 @@ sample_phi <- function(X){
 }
 
 MH_Gibbs <- function(n,init){
-  X <- matrix(NA,n,13)
-  X[1] <- init
+  X <- rep(list(init),n)
   for (i in 1:n){
-    for (k in 1:8){
-      X[i] <- sample_beta(k,X)
+    for (k in 1:8){ 
+      X[[i]] <- sample_beta(k,X[[i]])
     }
-    X[i] <- sample_alpha(X)
-    X[i] <- sample_theta(X)
-    X[i] <- sample_phi(X)
+    X[[i]] <- sample_alpha(X[[i]])
+    X[[i]] <- sample_theta(X[[i]])
+    X[[i]] <- sample_phi(X[[i]])
   }
   return (X)
 }
